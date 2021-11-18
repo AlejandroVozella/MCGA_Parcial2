c84a8fdde4854737501ad23152834184edf00026
Alejandro Vozella <alejandrovozella@gmail.com>
2021-11-18 19:50:26 -0300

    Setup del Proyecto

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './style.css';
+const Footer = ()=>{
+    return(
+        <div className="footer">
+            <h3>Manuel Verolo, Cesar Pagliarini ,Nicolas Vozella</h3>
+        </div>
+    )
+}
+
+export default Footer
