d3a052c5a9e116eab71de77d18c2f7a67cb7068a
Alejandro Vozella <alejandrovozella@gmail.com>
2021-11-22 08:21:58 -0300

    Modificacion Footer

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -3,6 +3,7 @@ const Footer = ()=>{
     return(
         <div className="footer">
             <h3>Manuel Verolo, Cesar Pagliarini ,Nicolas Vozella</h3>
+            <p>Copyright &copy; 2021</p>
         </div>
     )
 }
