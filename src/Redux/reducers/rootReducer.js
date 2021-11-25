f913d8a048ea2146a5a401d8bfe81b1ae33f5869
Ruben Gustavo Sato <rubensato@gmail.com>
2021-11-25 19:56:20 -0300

    Revert "Feature clientes recepcionistas"

--- src/Redux/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';                   
-import reducersCabanas from './reducersCabanas';           // Reducers de Cabañas
-
-const rootReducer = combineReducers ({                  // combina todos los reducers de la aplicacion
-
-    cabanas: reducersCabanas
-});
-
-export default rootReducer;
